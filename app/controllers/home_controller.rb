361c8f65a6813ed2d8d1444b52917b4fbc94f191
Luciano Maiwald <luciano.maiwald@gmail.com>
2015-11-27 12:01:16 +0100

    Initial commit

---
+++ app/controllers/home_controller.rb
@@ -0,0 +1,5 @@
+class HomeController < ApplicationController
+  def index
+    @venue = "Students On Rails"
+  end
+end
---
+++ app/models/user.rb
@@ -0,0 +1,6 @@
+class User < ActiveRecord::Base
+  # Include devise modules. Others available are:
+  # :confirmable, :lockable, :recoverable, :timeoutable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :rememberable, :trackable, :validatable
+end
