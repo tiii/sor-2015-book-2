4e8303ab8a39c4167492a5e60cfd0b71db864abd
Felix Jensen <engineduck@posteo.de>
2015-11-29 14:22:28 +0100

    fixed negative quantity in cart

--- app/controllers/books_cart_controller.rb
+++ app/controllers/books_cart_controller.rb
@@ -4,7 +4,9 @@ class BooksCartController < ApplicationController
   def update
     @books_cart = BooksCart.find(params[:id])
 
-    if @books_cart.update(books_cart_params)
+    if params["books_cart"]["quantity"].to_i < 1
+      redirect_to @books_cart.cart, alert: 'Die Buch-Anzahl darf nicht kleiner als 1 sein!'
+    elsif @books_cart.update(books_cart_params)
       redirect_to @books_cart.cart, notice: 'Buch-Anzahl aktualisiert.'
     else
       redirect_to @books_cart.cart, alert: 'Die Buch-Anzahl konnte nicht geändert werden.'
--- app/models/books_cart.rb
+++ app/models/books_cart.rb
@@ -2,7 +2,8 @@ class BooksCart < ActiveRecord::Base
 
   belongs_to :book
   belongs_to :cart
-  
+
+  validates :quantity, numericality: { only_integer: true, greater_than: 0}  
 
   def sum
     book.price * quantity
